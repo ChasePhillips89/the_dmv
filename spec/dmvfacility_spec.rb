1a888b5127a846031135cc202aee9650e0943e50
Chase Phillips <chase.thomas.phillips@gmail.com>
2024-12-16 18:56:45 -0500

    finished tests in spec file and matching class methods

--- spec/dmvfacility_spec.rb
+++ spec/dmvfacility_spec.rb
@@ -29,10 +29,11 @@ RSpec.describe DMVFacility do
     first_office_data = co_dmv_office_locations.first
     first_facility = @facility.facilities.first
 
-    expect(first_facility).to be_a(Facility) 
-    expect(first_facility.name).to eq(first_office_data[:dmv_office])
-    expect(first_facility.address).to eq(first_office_data[:address_li])
-    expect(first_facility.phone).to eq(first_office_data[:phone])
+    expect(@facility.facilities[0].name).to eq("DMV Tremont Branch")
+    expect(@facility.facilities[0].address).to eq("2855 Tremont Place")
+    expect(@facility.facilities[0].phone).to eq("(720) 865-4600")
+     
+    
   end
   
     
